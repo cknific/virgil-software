5a9c8718c40816725cfdd3bf21ddd1a5ac3ece19
Chris Knific <cknific@gmail.com>
2020-05-08 20:16:02 -0700

    Add gatsby-ssr.js content

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,24 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
+
+exports.replaceRenderer = ({
+  bodyComponent,
+  replaceBodyHTMLString,
+  setHeadComponents,
+}) => {
+  const serverStyleSheet = new ServerStyleSheet()
+
+  replaceBodyHTMLString(
+    renderToString(
+      <StyleSheetManager sheet={serverStyleSheet.instance}>
+        {bodyComponent}
+      </StyleSheetManager>
+    )
+  )
+
+  setHeadComponents([serverStyleSheet.getStyleElement()])
+}
