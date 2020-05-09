4fd269990c163e5eaacffb3c49b293808e6543e9
Chris Knific <cknific@gmail.com>
2020-05-08 21:28:16 -0700

    Remove construction image

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,7 +1,6 @@
 import { Container } from 'reactstrap'
 import React from 'react'
 import styled from 'styled-components'
-import Image from './image'
 
 const Wrapper = styled.div`
   background-color: #18bc9c;
@@ -13,18 +12,10 @@ const StyledDiv = styled.div`
   max-width: 40rem;
 `
 
-const FlexContainer = styled.div`
-  align-items: center;
-  display: flex;
-`
-
 const Hero = () => (
   <Wrapper>
     <Container>
-      <FlexContainer>
-        <Image />
-        <h1 className="display-4">&nbsp;Site Under Construction</h1>
-      </FlexContainer>
+      <h1 className="display-4">Site Under Construction</h1>
       <StyledDiv>
         <p className="lead">Meanwhile, Virgil Software continues to provide software product management, front-end development, and UX/UI design.</p>
       </StyledDiv>
