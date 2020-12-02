461555d445e1c1e745eb3b2cf532117fbe969311
Chris Knific <chris@mutations.ltd>
2020-12-01 20:57:42 -0800

    Update site metadata again

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Chris Knific | Virgil Software`,
+    title: `Chris Knific`,
     description: `Chris Knific: Virgil Software Consulting.`,
     author: `Virgil Software`,
   },
