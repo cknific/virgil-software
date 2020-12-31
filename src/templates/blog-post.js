c2f40e45e0087327878b705e13a409fec534793c
Chris Knific <chris@mutations.ltd>
2020-12-30 22:05:23 -0800

    Update post template layout

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -15,8 +15,11 @@ const PostHero = styled.div`
   margin-bottom: 4rem;
 `
 
-const PostBody = styled.div`
-  margin-bottom: 4rem;
+const PostBody = styled(Container)`
+  && {
+    margin-bottom: 4rem;
+    max-width: 950px;
+  }
 `
 
 const Breadcrumb = styled.div`
@@ -48,19 +51,17 @@ export default function Template ({ data }) {
         </Container>
       </PostHero>
       <PostBody>
-        <Container>
-          <Link to="/blog">
-            <Breadcrumb>
-              <IconContainer>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z" />
-                </svg>
-              </IconContainer>
-              View all Blog Posts
-            </Breadcrumb>
-          </Link>
-          <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        </Container>
+        <Link to="/blog">
+          <Breadcrumb>
+            <IconContainer>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z" />
+              </svg>
+            </IconContainer>
+            View all Blog Posts
+          </Breadcrumb>
+        </Link>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </PostBody>
     </Layout>
   )
