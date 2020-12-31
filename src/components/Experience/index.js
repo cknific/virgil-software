b52c1761cc6f12720a219eb5cfa8acce8a86d7d4
Chris Knific <chris@mutations.ltd>
2020-12-30 19:48:37 -0800

    Update Experience briefcase icon

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import styled from 'styled-components'
 import { Container } from 'reactstrap'
 import { YearsActive, SectionHeading } from '../Shared'
-import Briefcase from './briefcase.svg'
 
 const Wrapper = styled.div`
   background-color: #fafafa;
@@ -15,8 +14,11 @@ const FlexContainer = styled.div`
   margin-bottom: 1.75rem;
 `
 
-const StyledImage = styled.img`
+const IconContainer = styled.div`
   margin-right: 0.5rem;
+  height: 1.5rem;
+  width: 1.5rem;
+  color: #adadad;
 `
 
 const Position = styled.h6`
@@ -41,7 +43,11 @@ const Experience = () => (
   <Wrapper>
     <Container>
       <FlexContainer>
-        <StyledImage src={Briefcase} />
+        <IconContainer>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+          </svg>
+        </IconContainer>
         <SectionHeading>Work Experience</SectionHeading>
       </FlexContainer>
       <JobContainer>
