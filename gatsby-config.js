b54c3ceeca0ad199d96566b9158fe031643f9287
Chris Knific <chris@mutations.ltd>
2020-12-31 22:45:23 -0800

    Add gatsby plugin styled components and babel plugin styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     `gatsby-plugin-catch-links`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
